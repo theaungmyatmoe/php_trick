5580bd283e16fb93e62f43f8635cc8714a1ad86d
aungmyatmoeedu <aungmyatmoe.edu@gmail.com>
2020-08-11 10:25:41 +0630

    phpaws 1.0

---
+++ awsphp.php
@@ -0,0 +1,24 @@
+<?php
+/*
+You can see test awsuserdomainname,this is not work for You
+You should use real url* :)
+*/
+$xml = simplexml_load_file("https://awsuserdomain.s3-ap-southeast-1.amazonaws.com/");
+$json = json_encode($xml);
+#decode json file as array
+$decode = json_decode($json,true);
+$i = 0;
+//echo print_r($decode); 
+#(To know json or array)
+#loop to get content
+foreach ($decode as $key=>$value){
+foreach ($value as $k=>$v){
+  $link = $v["Key"]."<br>";
+?>
+<!-- I use php embed system -->
+<h6><? echo $link; ?></h6>
+<a href="https://awsuserdomain.s3-ap-southeast-1.amazonaws.com/<? echo $link; ?>"><? echo $link; ?></a>
+<?php
+}
+}
+?>
